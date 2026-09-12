6f0a4b7d9c05519b049143664329e26d518a551a
hfzkhan50-sudo <hfzkhan50@gmail.com>
2026-09-12 16:51:54 +0500

    Update app.config.js

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -3,6 +3,8 @@ export default {
     name: 'BKR Fast Food Monitor',
     slug: 'bkr-fast-food-monitor',
     version: '1.0.0',
+    android: { package: 'com.bkr.fastfood.monitor' },
+    ios: { bundleIdentifier: 'com.bkr.fastfood.monitor' },
     extra: {
       apiUrl: process.env.EXPO_PUBLIC_API_URL || 'https://bkr-fastfood-backend-1zbb.onrender.com/api'
     }
